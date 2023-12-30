edc7c3057d43a2e065319f3f2fc7c2566cc92e2c
khang.duong/PC <dvkhangnt@gmail.com>
2023-12-30 12:10:00 +0700

    IMPROVE - Clean code.

--- redrock-template-libgdx/core/src/com/redrock/Main.java
+++ redrock-template-libgdx/core/src/com/redrock/Main.java
@@ -213,7 +213,7 @@ public class Main extends Game implements IQuit {
 		sceneMgr			= new SceneMgr(this);
 
 		boolean newbie = session.profile.newbie;
-		sceneMgr.changeScene(SceneMgr.GAMEPLAY_SCENE);
+		sceneMgr.changeScene(SceneMgr.START_SCENE);
 		debugFps();
 	}
 
--- redrock-template-libgdx/core/src/com/redrock/scenes/StartScene.java
+++ redrock-template-libgdx/core/src/com/redrock/scenes/StartScene.java
@@ -14,72 +14,71 @@ public class StartScene extends ScreenAdapter {
   public static float CENTER_Y = Main.getStage().getHeight()/2;
 
   private final Group gParent;
+  private Image settingButton;
+  private Group startButton, modeButton;
 
   public StartScene() {
     this.gParent = new Group();
     this.gParent.setSize(Main.getStage().getWidth(), Main.getStage().getHeight());
 
     Main.layers().init(SceneMgr.START_SCENE, Main.getStage().getWidth(), Main.getStage().getHeight());
+
+    this.initUIElements();
+    this.initEventListeners();
   }
 
-  @Override
-  public void show() {
-    super.show();
-
-    Main.layers().activeLayersBy(SceneMgr.START_SCENE);
-    Main.getStage().addActor(gParent);
-
-
+  private void initUIElements(){
     Image background = new Image(Main.asset().getTG("background"));
-    this.gParent.addActor(background);
     Image logo = new Image(Main.asset().getTG("logo"));
-    this.gParent.addActor(logo);
-    logo.setPosition(58,871);
+    this.settingButton = new Image(Main.asset().getTG("setting_btn"));
 
-    Image settingButton = new Image(Main.asset().getTG("setting_btn"));
+    this.gParent.addActor(background);
+    this.gParent.addActor(logo);
     this.gParent.addActor(settingButton);
 
+    logo.setPosition(58,871);
     settingButton.setPosition(605,1152);
 
-    settingButton.addListener(new ClickListener(){
+    this.initButtons();
+  }
+
+  private void initButtons(){
+    this.startButton = new Group();
+    this.modeButton = new Group();
+
+    Image greenButtonShape = new Image(Main.asset().getTG("btn_green"));
+    Image startText = new Image(Main.asset().getTG("Start"));
+    Image orangeButtonShape = new Image(Main.asset().getTG("btn_orange"));
+    Image modeText = new Image(Main.asset().getTG("Mode"));
+
+    this.startButton.addActor(greenButtonShape);
+    this.startButton.addActor(startText);
+    this.modeButton.addActor(orangeButtonShape);
+    this.modeButton.addActor(modeText);
+
+    this.startButton.setSize(greenButtonShape.getWidth(), greenButtonShape.getHeight());
+    this.modeButton.setSize(orangeButtonShape.getWidth(), orangeButtonShape.getHeight());
+
+    startText.setPosition((startButton.getWidth() - startText.getWidth())/2,
+            (startButton.getHeight() - startText.getHeight())/2);
+    modeText.setPosition((modeButton.getWidth() - modeText.getWidth())/2,
+            (modeButton.getHeight() - modeText.getHeight())/2);
+
+    this.startButton.setPosition(231, 496);
+    this.modeButton.setPosition(231, 331);
+
+    this.gParent.addActor(this.startButton);
+    this.gParent.addActor(this.modeButton);
+  }
+
+  private void initEventListeners(){
+     this.settingButton.addListener(new ClickListener(){
       public void clicked(InputEvent event, float x, float y) {
         super.clicked(event, x, y);
 
         System.out.println("clicked settingButton");
       }
     });
-
-    Group startButton = new Group();
-    Image greenButtonShape = new Image(Main.asset().getTG("btn_green"));
-    Image startText = new Image(Main.asset().getTG("Start"));
-    startButton.addActor(greenButtonShape);
-    startButton.addActor(startText);
-    this.gParent.addActor(startButton);
-
-    startButton.setSize(greenButtonShape.getWidth(), greenButtonShape.getHeight());
-    startButton.debug();
-
-    startText.setPosition((startButton.getWidth() - startText.getWidth())/2,
-            (startButton.getHeight() - startText.getHeight())/2);
-
-    startButton.setPosition(231, 496);
-
-    Group modeButton = new Group();
-    Image orangeButtonShape = new Image(Main.asset().getTG("btn_orange"));
-    Image modeText = new Image(Main.asset().getTG("Mode"));
-    modeButton.addActor(orangeButtonShape);
-    modeButton.addActor(modeText);
-    this.gParent.addActor(modeButton);
-
-    modeButton.setSize(greenButtonShape.getWidth(), greenButtonShape.getHeight());
-    modeButton.debug();
-
-    modeText.setPosition((startButton.getWidth() - startText.getWidth())/2,
-            (startButton.getHeight() - startText.getHeight())/2);
-
-    modeButton.setPosition(231, 331);
-
-
 
     startButton.addListener(new ClickListener(){
       public void clicked(InputEvent event, float x, float y) {
@@ -97,14 +96,14 @@ public class StartScene extends ScreenAdapter {
         System.out.println("clicked modeButton");
       }
     });
-
-
   }
 
   @Override
-  public void render(float delta) {
-    super.render(delta);
+  public void show() {
+    super.show();
 
+    Main.layers().activeLayersBy(SceneMgr.START_SCENE);
+    Main.getStage().addActor(gParent);
   }
 
   @Override
